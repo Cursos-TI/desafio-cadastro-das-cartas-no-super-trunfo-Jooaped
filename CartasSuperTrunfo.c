40050725157ae527f4ff0f018d9f7735301e6cf8
João pedro <jpalves1511@gmail.com>
2025-03-28 04:39:07 +0000

    inicio do codigo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,4 +3,13 @@
 int main(){
 
     printf("desafo super trunfo!\n");
+    int população;
+ float area;
+ float pib;
+ int numero_de_pontos_turisticos;
+
+printf("digite a população: \n");
+scanf("%d",&população);
+
+return 0;
 }
